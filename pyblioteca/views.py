1697d5dda9c2f4b1f2f45d9a022b193d0cfe1ef1
Marcos Serra <marcosserra133@gmail.com>
2023-12-05 17:01:54 -0300

    update

--- pyblioteca/views.py
+++ pyblioteca/views.py
@@ -1,11 +1,17 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+
+from utils.livros.factory import make_livros
 
 # Create your views here.
 
 def home(request):
-    return render(request,'pyblioteca/pages/home.html')
+    return render(request,'pyblioteca/pages/home.html', context={
+        'livros': [make_livros() for _ in range(10)],
+    })
 
 
 def livros(request, id):
-    return render(request,'pyblioteca/pages/livros-view.html')
+    return render(request,'pyblioteca/pages/livros-view.html', context={
+        'livro': make_livros(),
+        'is_detail_page': True,
+    })
