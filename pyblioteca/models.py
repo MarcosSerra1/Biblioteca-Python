8ab5f5bfb6a2546351c2adccaa25a881962c8388
Marcos Serra <marcosserra133@gmail.com>
2023-12-15 20:02:15 -0300

    Update models.py

--- pyblioteca/models.py
+++ pyblioteca/models.py
@@ -4,19 +4,28 @@ from django.db import models
 class Autor(models.Model):
     nome = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.nome
+
 
 class Genero(models.Model):
     genero = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.genero
 
 
 class Livros(models.Model):
     titulo = models.CharField(max_length=200)
     descricao = models.TextField()
-    tempo_de_leitura = models.IntegerField()
+    tempo_de_leitura = models.CharField(max_length=100)
     quantidade_de_livros = models.IntegerField()
     capitulos = models.IntegerField()
-    lancado_em = models.DateField()
+    data_de_lancamento = models.DateField()
     esta_emprestado = models.BooleanField(default=False)
     capa = models.ImageField(upload_to='livros/capas/%Y/%m/%d/', null=True, blank=True)
     autor = models.ForeignKey(Autor, on_delete=models.SET_NULL, null=True)
     genero = models.ForeignKey(Genero, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.titulo
